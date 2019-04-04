84bea957145fbbe165123468f365188e9d002b2a
Thiago Henrique <ogaihto2728@gmail.com>
2019-04-04 18:04:40 -0300

    v1

---
+++ TrabalhoBimestral/app/src/main/java/com/ogaihto/trabalhobimestral/item_task.kt
@@ -0,0 +1,12 @@
+package com.ogaihto.trabalhobimestral
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class item_task : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_task)
+    }
+}
