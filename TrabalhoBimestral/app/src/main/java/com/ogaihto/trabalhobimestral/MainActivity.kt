0d863d0cfcb64d50daea1f29d56ed26b79a4f12b
Thiago Henrique <ogaihto2728@gmail.com>
2019-04-05 11:29:44 -0300

    v2

--- TrabalhoBimestral/app/src/main/java/com/ogaihto/trabalhobimestral/MainActivity.kt
+++ TrabalhoBimestral/app/src/main/java/com/ogaihto/trabalhobimestral/MainActivity.kt
@@ -2,11 +2,49 @@ package com.ogaihto.trabalhobimestral
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ogaihto.trabalhobimestral.R.id.ListTask
+import com.ogaihto.trabalhobimestral.R.id.plus
+import com.ogaihto.trabalhobimestral.entities.Task
+import com.ogaihto.trabalhobimestral.ui.TaskAdapter
+import com.ogaihto.trabalhobimestral.ui.TaskListListener
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 
-class MainActivity : AppCompatActivity() {
+
+
+class MainActivity : AppCompatActivity(), TaskListListener {
+    override fun removedTask(task: Task) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    lateinit var adapter: TaskAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        plus.setOnClickListener{
+            click()
+        }
+
+        adapter = TaskAdapter(mutableListOf(), this)
+        ListTask.adapter = adapter
+
+        ListTask.layoutManager = LinearLayoutManager(
+            this, RecyclerView.VERTICAL, false
+        )
+
+    }
+
+    private fun click() {
+        val task = Task("","")
+
+        adapter.adicionarTask(task)
+
+        plus.hide()
     }
 }
